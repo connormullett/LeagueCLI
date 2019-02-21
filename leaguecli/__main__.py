7e3f51f5b4a201c4bce9170e15460dbca5186c2c
connormullett <connormullett@gmail.com>
2019-02-21 12:13:07 -0500

    final scaffolds before implemention of api

--- leaguecli/__main__.py
+++ leaguecli/__main__.py
@@ -4,6 +4,8 @@ import click
 import requests
 
 
+BASE_URL = 'https://na1.api.riotgames.com/lol/'
+
 def get_dev_key():
     with open('settings.txt', 'r') as f:
         return f.read().rstrip()
@@ -11,13 +13,18 @@ def get_dev_key():
 
 @click.group()
 @click.option('--api', '-a', help='your riot games api key')
+@click.option('--verbose', '-v', help='enable verbosity')
 def main(api):
-    pass
+    api = get_dev_key() if api is None else api
+    click.echo(api)
 
 
 @main.command()
-def challenger():
-    pass
+@click.option('--queue', '-q', help='queue type to search for', default='s')
+@click.option('--limit', '-l', help='limit returned results starting from rank 1 ' \
+        'to limit')
+def challenger(queue):
+    click.echo(queue)
 
 
 @main.command()
@@ -26,7 +33,8 @@ def rank():
 
 
 @main.command()
-def summoner():
-    pass
+@click.argument('name', nargs=1)
+def summoner(name):
+    click.echo(name)
 
 
